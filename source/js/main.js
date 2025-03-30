7dc3289994295be68a6bb86e4078ad8943651fa5
Alina <alinakubikalex@gmail.com>
2025-03-30 19:07:34 +0200

    add swiper part1

--- source/js/main.js
+++ source/js/main.js
@@ -1,8 +1,45 @@
 // https://swiperjs.com/get-started#installation
-// import Swiper from "swiper";
-// import {Navigation, Pagination} from "swiper/modules";
-// import 'swiper/css';
+import Swiper from 'swiper';
 import './about';
 import './price';
 import './faq';
 
+// function initSwipers() {
+//   const juriSwiper = new Swiper('.juri__swiper', {
+//     loop: true,
+//     setWrapperSize: true,
+//     width: 'auto',
+//     slidesPerView: 'auto',
+//     spaceBetween: 0,
+//     simulateTouch: true,
+//     touchRatio: 1,
+//     grabCursor: true,
+//     freeMode: true,
+//     speed: 0,
+//     oneWayMovement: false,
+//     navigation: {
+//       nextEl: '.juri__button--next',
+//       prevEl: '.juri__button--previous',
+//     },
+//   });
+
+//   const reviewSwiper = new Swiper('.reviews__swiper', {
+//     loop: true,
+//     setWrapperSize: true,
+//     width: 'auto',
+//     slidesPerView: 'auto',
+//     spaceBetween: 0,
+//     simulateTouch: true,
+//     touchRatio: 1,
+//     grabCursor: true,
+//     freeMode: true,
+//     speed: 0,
+//     oneWayMovement: false,
+//     navigation: {
+//       nextEl: '.juri__button--next',
+//       prevEl: '.juri__button--previous',
+//     },
+//   });
+// }
+
+// document.addEventListener('DOMContentLoaded', initSwipers);
